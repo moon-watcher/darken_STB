c898e38899a96d74d9d18ab307ce622b79f30643
Moon-Watcher <langares@yahoo.es>
2026-08-12 13:16:29 +0200

    vvv

--- src/darken.h
+++ src/darken.h
@@ -7,6 +7,7 @@
 #define _DE_CONCAT(A, B) A##B
 #define _DE_UNIQUE(A, B) _DE_CONCAT(A, B)
 #define _DE_ENTITY_STRIDE(PAYLOAD) _DE_ALIGN4(sizeof(struct de_entity) + (PAYLOAD))
+#define _DE_GET_MACRO(_1, _2, _3, _4, _5, _6, _7, NAME, ...) NAME
 
 typedef void *(*de_state)(void *);
 
@@ -102,7 +103,7 @@ uint16_t de_system_remove(de_system *, void *);
     void *_DE_UNIQUE(_sp_, __LINE__)[(CAPACITY) * (PARAMS)]; \
     de_system_init((SYS), _DE_UNIQUE(_sp_, __LINE__), (CAPACITY), (PARAMS))
 
-#define _DE_SYS_ADD(SYS, N, ...)             \
+#define _DE_SYSTEM_ADD(SYS, N, ...)          \
     ({                                       \
         de_system *_s = (SYS);               \
         uint16_t _ok = 0;                    \
@@ -116,39 +117,32 @@ uint16_t de_system_remove(de_system *, void *);
         _ok;                                 \
     })
 
-#define _DE_SYS_ADD1(SYS, A) \
-    _DE_SYS_ADD(SYS, 1, _p[0] = (void *)(A);)
+#define _DE_SYSTEM_ADD1(SYS, A) \
+    _DE_SYSTEM_ADD(SYS, 1, _p[0] = (void *)(A);)
 
-#define _DE_SYS_ADD2(SYS, A, B) \
-    _DE_SYS_ADD(SYS, 2, _p[0] = (void *)(A); _p[1] = (void *)(B);)
+#define _DE_SYSTEM_ADD2(SYS, A, B) \
+    _DE_SYSTEM_ADD(SYS, 2, _p[0] = (void *)(A); _p[1] = (void *)(B);)
 
-#define _DE_SYS_ADD3(SYS, A, B, C) \
-    _DE_SYS_ADD(SYS, 3, _p[0] = (void *)(A); _p[1] = (void *)(B); _p[2] = (void *)(C);)
+#define _DE_SYSTEM_ADD3(SYS, A, B, C) \
+    _DE_SYSTEM_ADD(SYS, 3, _p[0] = (void *)(A); _p[1] = (void *)(B); _p[2] = (void *)(C);)
 
-#define _DE_SYS_ADD4(SYS, A, B, C, D) \
-    _DE_SYS_ADD(SYS, 4, _p[0] = (void *)(A); _p[1] = (void *)(B); _p[2] = (void *)(C); _p[3] = (void *)(D);)
+#define _DE_SYSTEM_ADD4(SYS, A, B, C, D) \
+    _DE_SYSTEM_ADD(SYS, 4, _p[0] = (void *)(A); _p[1] = (void *)(B); _p[2] = (void *)(C); _p[3] = (void *)(D);)
 
-#define _DE_SYS_ADD5(SYS, A, B, C, D, E) \
-    _DE_SYS_ADD(SYS, 5, _p[0] = (void *)(A); _p[1] = (void *)(B); _p[2] = (void *)(C); _p[3] = (void *)(D); _p[4] = (void *)(E);)
+#define _DE_SYSTEM_ADD5(SYS, A, B, C, D, E) \
+    _DE_SYSTEM_ADD(SYS, 5, _p[0] = (void *)(A); _p[1] = (void *)(B); _p[2] = (void *)(C); _p[3] = (void *)(D); _p[4] = (void *)(E);)
 
-#define _DE_SYSTEM_ADD_GET_MACRO(_1, _2, _3, _4, _5, _6, NAME, ...) NAME
+#define de_system_add(...)                                           \
+    _DE_GET_MACRO(__VA_ARGS__, _,                                    \
+                  _DE_SYSTEM_ADD5, _DE_SYSTEM_ADD4, _DE_SYSTEM_ADD3, \
+                  _DE_SYSTEM_ADD2, _DE_SYSTEM_ADD1, unused)(__VA_ARGS__)
 
-#define de_system_add(...)                                             \
-    _DE_SYSTEM_ADD_GET_MACRO(__VA_ARGS__,                              \
-                             _DE_SYS_ADD5, _DE_SYS_ADD4, _DE_SYS_ADD3, \
-                             _DE_SYS_ADD2, _DE_SYS_ADD1, unused)(__VA_ARGS__)
-
-#define _DE_SYSTEM_GET_MACRO(_1, _2, _3, _4, _5, _6, _7, NAME, ...) NAME
-
-#define de_system_foreach(...)                                                             \
-    _DE_SYSTEM_GET_MACRO(__VA_ARGS__,                                                      \
-                         _DE_SYSTEM_FOREACH_5, _DE_SYSTEM_FOREACH_4, _DE_SYSTEM_FOREACH_3, \
-                         _DE_SYSTEM_FOREACH_2, _DE_SYSTEM_FOREACH_1, _DE_SYSTEM_FOREACH_0)(__VA_ARGS__)
-
-#define de_system_iterator(...)                                                               \
-    _DE_SYSTEM_GET_MACRO(__VA_ARGS__,                                                         \
-                         _DE_SYSTEM_ITERATOR_5, _DE_SYSTEM_ITERATOR_4, _DE_SYSTEM_ITERATOR_3, \
-                         _DE_SYSTEM_ITERATOR_2, _DE_SYSTEM_ITERATOR_1, _DE_SYSTEM_ITERATOR_0)(__VA_ARGS__)
+#define _DE_SYSTEM_ITERATOR(NAME, FOREACH, ...) \
+    void *NAME(de_system *system)               \
+    {                                           \
+        FOREACH(system, __VA_ARGS__);           \
+        return (void *)de_state_loop;           \
+    }
 
 #define _DE_SYSTEM_FOREACH(SYSTEM, IT)                     \
     void **items = (SYSTEM)->pool;                         \
@@ -173,47 +167,33 @@ uint16_t de_system_remove(de_system *, void *);
 #define _DE_SYSTEM_FOREACH_5(SYSTEM, A, B, C, D, E, IT) \
     _DE_SYSTEM_FOREACH(SYSTEM, { A = items[i++]; B = items[i++]; C = items[i++]; D = items[i++]; E = items[i++]; IT; })
 
-#define _DE_SYSTEM_ITERATOR_0(NAME, IT)   \
-    void *NAME(de_system *system)         \
-    {                                     \
-        _DE_SYSTEM_FOREACH_0(system, IT); \
-        return (void *)de_state_loop;     \
-    }
+#define _DE_SYSTEM_ITERATOR_0(NAME, IT) \
+    _DE_SYSTEM_ITERATOR(NAME, _DE_SYSTEM_FOREACH_0, IT)
 
-#define _DE_SYSTEM_ITERATOR_1(NAME, A, IT)   \
-    void *NAME(de_system *system)            \
-    {                                        \
-        _DE_SYSTEM_FOREACH_1(system, A, IT); \
-        return (void *)de_state_loop;        \
-    }
+#define _DE_SYSTEM_ITERATOR_1(NAME, A, IT) \
+    _DE_SYSTEM_ITERATOR(NAME, _DE_SYSTEM_FOREACH_1, A, IT)
 
-#define _DE_SYSTEM_ITERATOR_2(NAME, A, B, IT)   \
-    void *NAME(de_system *system)               \
-    {                                           \
-        _DE_SYSTEM_FOREACH_2(system, A, B, IT); \
-        return (void *)de_state_loop;           \
-    }
+#define _DE_SYSTEM_ITERATOR_2(NAME, A, B, IT) \
+    _DE_SYSTEM_ITERATOR(NAME, _DE_SYSTEM_FOREACH_2, A, B, IT)
 
-#define _DE_SYSTEM_ITERATOR_3(NAME, A, B, C, IT)   \
-    void *NAME(de_system *system)                  \
-    {                                              \
-        _DE_SYSTEM_FOREACH_3(system, A, B, C, IT); \
-        return (void *)de_state_loop;              \
-    }
+#define _DE_SYSTEM_ITERATOR_3(NAME, A, B, C, IT) \
+    _DE_SYSTEM_ITERATOR(NAME, _DE_SYSTEM_FOREACH_3, A, B, C, IT)
 
-#define _DE_SYSTEM_ITERATOR_4(NAME, A, B, C, D, IT)   \
-    void *NAME(de_system *system)                     \
-    {                                                 \
-        _DE_SYSTEM_FOREACH_4(system, A, B, C, D, IT); \
-        return (void *)de_state_loop;                 \
-    }
+#define _DE_SYSTEM_ITERATOR_4(NAME, A, B, C, D, IT) \
+    _DE_SYSTEM_ITERATOR(NAME, _DE_SYSTEM_FOREACH_4, A, B, C, D, IT)
 
-#define _DE_SYSTEM_ITERATOR_5(NAME, A, B, C, D, E, IT)   \
-    void *NAME(de_system *system)                        \
-    {                                                    \
-        _DE_SYSTEM_FOREACH_5(system, A, B, C, D, E, IT); \
-        return (void *)de_state_loop;                    \
-    }
+#define _DE_SYSTEM_ITERATOR_5(NAME, A, B, C, D, E, IT) \
+    _DE_SYSTEM_ITERATOR(NAME, _DE_SYSTEM_FOREACH_5, A, B, C, D, E, IT)
+
+#define de_system_foreach(...)                                                      \
+    _DE_GET_MACRO(__VA_ARGS__,                                                      \
+                  _DE_SYSTEM_FOREACH_5, _DE_SYSTEM_FOREACH_4, _DE_SYSTEM_FOREACH_3, \
+                  _DE_SYSTEM_FOREACH_2, _DE_SYSTEM_FOREACH_1, _DE_SYSTEM_FOREACH_0)(__VA_ARGS__)
+
+#define de_system_iterator(...)                                                        \
+    _DE_GET_MACRO(__VA_ARGS__,                                                         \
+                  _DE_SYSTEM_ITERATOR_5, _DE_SYSTEM_ITERATOR_4, _DE_SYSTEM_ITERATOR_3, \
+                  _DE_SYSTEM_ITERATOR_2, _DE_SYSTEM_ITERATOR_1, _DE_SYSTEM_ITERATOR_0)(__VA_ARGS__)
 
 #endif
 
